6fe4f9c278d5af37c31b88d2e319a3c4abddd2e8
yueunfive <122276414+yueunfive@users.noreply.github.com>
2023-08-01 19:24:08 +0900

    profile deploy

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-    return (
-        <>
-            <div className="bg-back-gradation"></div>
-            <div className="bg-gradation"></div>
-            <div className="rotate">
-                <div className="card front">
-                    <div className="ssu_logo"></div>
-                    <h1>LIKELION</h1>
-                    <div className="likelion_wrapper">
-                        <div className="likelion_logo"></div>
-                        <p>11th</p>
-                    </div>
-                </div>
-                <div className="card back">
-                    <div>
-                        <span className="name">숭멋사</span>
-                        <span className="position">FE</span>
-                    </div>
-                    <a className="github"></a>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-back-gradation"></div>
+      <div className="bg-gradation"></div>
+      <div className="rotate">
+        <div className="card front">
+          <div className="ssu_logo"></div>
+          <h1>LIKELION</h1>
+          <div className="likelion_wrapper">
+            <div className="likelion_logo"></div>
+            <p>11th</p>
+          </div>
+        </div>
+        <div className="card back">
+          <div>
+            <span className="name">오유은</span>
+            <span className="position">FE</span>
+          </div>
+          <a className="github" href="https://github.com/yueunfive"></a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
